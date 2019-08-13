b6dc59dce881e28646b9d7560d0e7ad4123cb9aa
cowankc <cowankc92@live.com>
2019-08-12 22:10:05 -0500

    started seccond function for opponent selection

--- docs/assets/javascript/game.js
+++ docs/assets/javascript/game.js
@@ -1,13 +1,55 @@
 $( document ).ready(function() {
+
+    // variables 
     const teddy = $("#teddy");
     const abe = $("#abe");
     const george = $("#george");
-    const thomas = $("thomas");
+    const thomas = $("#thomas");
 
-    teddy.on("click", function () {
-        teddy.appendTo("#userspresident");
-    });
+    // functions
 
+    selectPrez = function () {
 
+        teddy.parent("#choices").on("click", function () {
+            teddy.appendTo("#userspresident");
+            abe.appendTo("#opponents");
+            george.appendTo("#opponents");
+            thomas.appendTo("#opponents");
+            
+        });
+
+        abe.parent("#choices").on("click", function () {
+            abe.appendTo("#userspresident");
+            teddy.appendTo("#opponents");
+            george.appendTo("#opponents");
+            thomas.appendTo("#opponents");
+        });
+
+        george.parent("#choices").on("click", function () {
+            george.appendTo("#userspresident");
+            abe.appendTo("#opponents");
+            teddy.appendTo("#opponents");
+            thomas.appendTo("#opponents");
+        });
+
+        thomas.parent("#choices").on("click", function () {
+            teddy.appendTo("#userspresident");
+            abe.appendTo("#opponents");
+            george.appendTo("#opponents");
+            thomas.appendTo("#opponents");
+        });
+    }
+
+    // selectOpponent = function () {
+    //     teddy.on("click", function () {
+    //     if (teddy.parent("opponents")) {
+    //         teddy.appendTo("#enemypresident");
+    //         };
+    //     });
+
+    
+
+    selectPrez ()
+    // selectOpponent ()
 
 });
