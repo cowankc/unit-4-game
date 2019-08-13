11b58c153ff7bb364c829398e054f9629f72fa2c
cowankc <cowankc92@live.com>
2019-08-13 17:22:14 -0500

    worked on opponent selection and tried to create a function using this

--- docs/assets/javascript/game.js
+++ docs/assets/javascript/game.js
@@ -9,45 +9,98 @@ $( document ).ready(function() {
     // functions
 
     selectPrez = function () {
-
-        teddy.parent("#choices").on("click", function () {
-            teddy.appendTo("#userspresident");
-            abe.appendTo("#opponents");
-            george.appendTo("#opponents");
-            thomas.appendTo("#opponents");
-            
+        $("prez").on("click", function () {
+            if (this.css("border", "5px solid blue")) {
+            this.appendTo("#userspresident");
+            this.css("border", "5px solid green")
+            !this.appendTo("#opponents");
+            !this.css("border", "5px solid red");
+            }            
         });
 
-        abe.parent("#choices").on("click", function () {
-            abe.appendTo("#userspresident");
-            teddy.appendTo("#opponents");
-            george.appendTo("#opponents");
-            thomas.appendTo("#opponents");
-        });
-
-        george.parent("#choices").on("click", function () {
-            george.appendTo("#userspresident");
-            abe.appendTo("#opponents");
-            teddy.appendTo("#opponents");
-            thomas.appendTo("#opponents");
-        });
-
-        thomas.parent("#choices").on("click", function () {
-            teddy.appendTo("#userspresident");
-            abe.appendTo("#opponents");
-            george.appendTo("#opponents");
-            thomas.appendTo("#opponents");
-        });
     }
+//         teddy.on("click", function () {
+//             if (teddy.css("border", "5px solid blue")) {
+//             teddy.appendTo("#userspresident");
+//             teddy.css("border", "5px solid green")
+//             abe.appendTo("#opponents");
+//             abe.css("border", "5px solid red");
+//             george.appendTo("#opponents");
+//             george.css("border", "5px solid red");
+//             thomas.appendTo("#opponents");
+//             thomas.css("border", "5px solid red");
+//             }            
+//         });
 
-    // selectOpponent = function () {
-    //     teddy.on("click", function () {
-    //     if (teddy.parent("opponents")) {
-    //         teddy.appendTo("#enemypresident");
-    //         };
-    //     });
+//         george.on("click", function () {
+//             if (george.css("border", "5px solid blue")) {
+//             george.appendTo("#userspresident");
+//             george.css("border", "5px solid green")
+//             abe.appendTo("#opponents");
+//             abe.css("border", "5px solid red");
+//             teddy.appendTo("#opponents");
+//             teddy.css("border", "5px solid red");
+//             thomas.appendTo("#opponents");
+//             thomas.css("border", "5px solid red");
+//             }
+//         });
 
-    
+//         abe.on("click", function () {
+//             if (abe.css("border", "5px solid blue")) {
+//             abe.appendTo("#userspresident");
+//             abe.css("border", "5px solid green")
+//             teddy.appendTo("#opponents");
+//             teddy.css("border", "5px solid red");
+//             george.appendTo("#opponents");
+//             george.css("border", "5px solid red");
+//             thomas.appendTo("#opponents");
+//             thomas.css("border", "5px solid red");  
+//             }          
+//         });
+
+//         thomas.on("click", function () {
+//             if (thomas.css("border", "5px solid blue")) {
+//             thomas.appendTo("#userspresident");
+//             thomas.css("border", "5px solid green")
+//             abe.appendTo("#opponents");
+//             abe.css("border", "5px solid red");
+//             george.appendTo("#opponents");
+//             george.css("border", "5px solid red");
+//             teddy.appendTo("#opponents");
+//             teddy.css("border", "5px solid red");   
+//             }         
+//         });
+
+//     }
+
+//     selectOpponent = function () {
+//         george.on("click", function () {
+//         if (george.css("border", "5px solid red")) {
+//             george.appendTo("#enemypresident");
+//             george.css("border", "5px solid orange")
+//         }
+//         });
+
+//         abe.on("click", function () {
+//             if (abe.css("border", "5px solid red")) {
+//                 abe.appendTo("#enemypresident");
+//                 abe.css("border", "5px solid orange")
+//             }
+//          });
+         
+//         teddy.on("click", function () {
+//             if (teddy.css("border", "5px solid red")) {
+//                 teddy.appendTo("#enemypresident");
+//                 teddy.css("border", "5px solid orange")
+//             }
+//         });
+//         thomas.on("click", function () {
+//             if (thomas.css("border", "5px solid red")) {
+//                 thomas.appendTo("#enemypresident");
+//                 thomas.css("border", "5px solid orange")
+//             }
+//         });
+// }  
 
     selectPrez ()
     // selectOpponent ()
