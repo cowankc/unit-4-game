54615841880ae3f1b73920a91822a122f450a809
cowankc <cowankc92@live.com>
2019-08-12 19:55:23 -0500

    Started working on jquery, was able to move image to div when clicked on

---
+++ docs/assets/javascript/game.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    const teddy = $("#teddy");
+    const abe = $("#abe");
+    const george = $("#george");
+    const thomas = $("thomas");
+
+    teddy.on("click", function () {
+        teddy.appendTo("#userspresident");
+    });
+
+
+
+});
