60aa7bc55d6e399060278b887fa99ae80d05e062
cowankc <cowankc92@live.com>
2019-08-14 18:37:15 -0500

    finished selection function!

--- docs/assets/javascript/game.js
+++ docs/assets/javascript/game.js
@@ -5,6 +5,7 @@ $( document ).ready(function() {
     const abe = $("#abe");
     const george = $("#george");
     const thomas = $("#thomas");
+    let chooseEnemy = false
 
     // functions
 
@@ -14,14 +15,27 @@ $( document ).ready(function() {
             const currentPrez = $(`#${selectedPrez.currentTarget.id}`);
             const classArray = [...selectedPrez.currentTarget.classList];
             currentPrez.addClass('green-border');
-            currentPrez.removeClass('blue-border')
+            currentPrez.removeClass('blue-border');
+            $(prez).appendTo("#opponents");
+            $(prez).addClass('red-border');
             currentPrez.appendTo("#userspresident");
+            chooseEnemy = true;
+            if(chooseEnemy =true) {
+                $(prez).on("click", function (selectedPrez2) {
+                    const currentPrez2 = $(`#${selectedPrez2.currentTarget.id}`);
+                    const classArray = [...selectedPrez.currentTarget.classList];
+                    currentPrez2.addClass('orange-border');
+                    currentPrez2.removeClass('red-border');
+                    currentPrez2.appendTo("#enemypresident");
+                    currentPrez.appendTo("#userspresident");
+                });
+               }
         });
-
     }
    
 
     selectPrez ()
+    
 
 
 });
