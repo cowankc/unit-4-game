bf3d8b94362203b6cbf5f7ea8454b4c97da63752
cowankc <cowankc92@live.com>
2019-08-17 20:53:30 -0500

    finished javascript, ready to deploy!

--- docs/assets/javascript/game.js
+++ docs/assets/javascript/game.js
@@ -3,35 +3,35 @@ $( document ).ready(function() {
     // variables 
     let teddy = $("#teddy")
     let teddyAttributes = {
-        health: 180,
-        attack: 12,
+        health: 200,
+        attack: 8,
         counter: 20
     };
     let abe = $("#abe")
     let abeAttributes =  {
         health: 140,
-        attack: 22,
+        attack: 17,
         counter: 25
     };
     let george = $("#george")
     let georgeAttributes =  {
-        health: 160,
-        attack: 18,
-        counter: 22,
+        health: 180,
+        attack: 10,
+        counter: 16,
     };
     let thomas = $("#thomas")
     let thomasAttributes =  {
         health: 100,
-        attack: 14,
+        attack: 15,
         counter: 50
     };
     let chooseEnemy = false
 
     let enemiesDefeated = 3
+
+
     // functions
-    
     selectPrez = function () {
-        chosenPrez = 15
         const prez = document.getElementsByClassName('prez');
         $(prez).on("click", function (selectedPrez) {
             if (chooseEnemy == false) {
@@ -71,39 +71,31 @@ $( document ).ready(function() {
             let baseAttack = 0;
 
             if ($("#teddy").hasClass('green-border')) {
-                console.log("teddy")
                 userAttributes = teddyAttributes
-                baseAttack = 12
+                baseAttack = 8
             }
             if ($("#george").hasClass('green-border')) {
-                console.log("george")
                 userAttributes = georgeAttributes
-                baseAttack = 18
+                baseAttack = 10
             }
             if ($("#thomas").hasClass('green-border')) {
-                console.log("thoms")
                 userAttributes = thomasAttributes
-                baseAttack = 14
+                baseAttack = 15
             }
             if ($("#abe").hasClass('green-border')) {
-                console.log("abe")
                 userAttributes = abeAttributes
-                baseAttack = 22
+                baseAttack = 17
             }
             if ($("#abe").hasClass('orange-border')) {
-                console.log("vs abe")
                 enemyAttributes = abeAttributes
             }
             if ($("#thomas").hasClass('orange-border')) {
-                console.log("vs thomas")
                 enemyAttributes = thomasAttributes
             }
             if ($("#george").hasClass('orange-border')) {
-                console.log("vs george")
                 enemyAttributes = georgeAttributes
             }
             if ($("#teddy").hasClass('orange-border')) {
-                console.log("vs teddy")
                 enemyAttributes = teddyAttributes
             }
             
@@ -116,7 +108,7 @@ $( document ).ready(function() {
             $("#userAttack").html(userAttributes.attack)
             $("#enemyHealth").html(enemyAttributes.health)
             $("#enemyAttack").html(enemyAttributes.counter)
-
+            
             if (enemyAttributes.health <= 0) {
                 $("#enemypresident").empty();
                 alert("You've defeated your opponent! Chose the next enemy")
@@ -131,14 +123,12 @@ $( document ).ready(function() {
                 alert("You weren't strong enough, try gain")
             }
             if (enemiesDefeated === 0) {
-                alert("NICE JOB! YOU WON!")
+                $("#enemypresident").html("Congrats you Won!")
             }
-            console.log(enemiesDefeated)
         });
     }
 
     
     selectPrez()
-    console.log(chooseEnemy);
     attackFunction()
 });
